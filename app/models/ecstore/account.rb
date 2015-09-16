ad84a917b258006931b60757e8254fff55e9bb45
叶思杰 <killernova@126.com>
2015-09-16 16:57:40 +0800

    by script

--- app/models/ecstore/account.rb
+++ app/models/ecstore/account.rb
@@ -6,13 +6,13 @@ class Ecstore::Account < Ecstore::Base
 	self.table_name = "sdb_pam_account"	
 
 	belongs_to :manager,:foreign_key=>"account_id"
-  	belongs_to :supplier, :foreign_key=>"supplier_id"
+	belongs_to :supplier, :foreign_key=>"supplier_id"
 
 	has_one :user, :foreign_key=>"member_id"
 	has_one :member,:foreign_key=>"member_id"
 	has_one :auth_ext, :foreign_key=>"account_id"
 
-  	has_many :commission,:foreign_key=>"member_id"
+	has_many :commission,:foreign_key=>"member_id"
 
 
 	attr_accessible :auth_ext_id, :login_name, :login_password,:account_type, :login_password_confirmation, :email, :mobile,:real_name,:discount_code, :follow_imodec,:license,:current_password
@@ -22,31 +22,31 @@ class Ecstore::Account < Ecstore::Base
 	validates :login_name, :presence=>{:presence=>true,:message=>"请填写用户名"}
 
 	validates :login_name, :length=>{:in=>3..50,:message=>"用户名应为3到50个字符"},
-						     :if=>Proc.new { |c| c.login_name.present? }
+	:if=>Proc.new { |c| c.login_name.present? }
 
 	validates :login_password,  :presence=>{:presence=>true,:message=>"请填写密码"}
 	validates :login_password,  :length=>{:minimum=>6,:message=>"密码不能少于6位"},
-								:if=>Proc.new{ |c| c.login_password.present? }
+	:if=>Proc.new{ |c| c.login_password.present? }
 
 	validates :login_password, :confirmation=>{:confirmation=>true,:message=>"两次密码不一致"}
 	#validates :login_password_confirmation,:presence=>{:presence=>true,:message=>"请填写确认密码"}
 	validates :email,:presence=>{:presence=>true,:message=>"请填写邮箱"}
 	validates :email,:format=>{:with=>/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,:message=>"邮箱格式不正确"},
-					    :if=>Proc.new{ |c| c.email.present? }
+	:if=>Proc.new{ |c| c.email.present? }
 
 	validates :mobile,:presence=>{:presence=>true,:message=>"请填写手机"}
 	validates :mobile,:format=>{:with=>/^\d{11}$/,:message=>"手机号码必须是11位数字"},
-					    :if=>Proc.new{ |c| c.mobile.present? }
+	:if=>Proc.new{ |c| c.mobile.present? }
 
-    validates :license, :presence=>{:presence=>true,:message=>"您还没有阅读注册协议"}, :if=>Proc.new{ |c| c.new_record? }
+	validates :license, :presence=>{:presence=>true,:message=>"您还没有阅读注册协议"}, :if=>Proc.new{ |c| c.new_record? }
 
 	validate :check_login_name_duplicated,:check_email_duplicated,:check_mobile_duplicated #:check_discount_code_availability
 
 	def check_login_name_duplicated
 		return if self.login_name.blank? || self.errors[:login_name].present?
 		if Ecstore::Account.find_by_login_name(self.login_name) ||
-		   Ecstore::User.find_by_mobile(self.login_name) ||
-		   Ecstore::User.find_by_email(self.login_name)
+			Ecstore::User.find_by_mobile(self.login_name) ||
+			Ecstore::User.find_by_email(self.login_name)
 			errors.add(:login_name, "用户名已经被使用！") if new_record?
 		end
 	end
@@ -59,15 +59,15 @@ class Ecstore::Account < Ecstore::Base
 	#end
 
 	def check_mobile_duplicated
-	    if self.mobile.present? and u = Ecstore::User.find_by_mobile(self.mobile) and  new_record?
-	      errors.add(:mobile, "手机号码已经被使用！")
-	    end
+		if self.mobile.present? and u = Ecstore::User.find_by_mobile(self.mobile) and  new_record?
+			errors.add(:mobile, "手机号码已经被使用！")
+		end
 	end
 
 	def check_email_duplicated
-	    if new_record? and self.email.present? and u = Ecstore::User.find_by_email(self.email)
-	      errors.add(:email, "邮箱已经被使用！")
-	    end
+		if new_record? and self.email.present? and u = Ecstore::User.find_by_email(self.email)
+			errors.add(:email, "邮箱已经被使用！")
+		end
 	end
 
 
@@ -113,7 +113,7 @@ class Ecstore::Account < Ecstore::Base
 			end
 		end
 		nil
-  end
+	end
 
   # def self.vshop_authenticate(name,password)
   #   account = self.where(:login_name=>name,:account_type=>"member").first
@@ -138,40 +138,40 @@ class Ecstore::Account < Ecstore::Base
     unless account
       #email
       if /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i =~ name
-        email = name
-        user = Ecstore::User.where(:email=>email,:email_validate=>'true',:supplier_id=>supplier_id).first
-        account = user.account if user
+      	email = name
+      	user = Ecstore::User.where(:email=>email,:email_validate=>'true',:supplier_id=>supplier_id).first
+      	account = user.account if user
 
       end
       #mobile
       if /^[1-9][0-9]{10}$/ =~ name
-        mobile = name
-        users = Ecstore::User.where(:mobile=>mobile,:sms_validate=>'true',:supplier_id=>supplier_id)
-        if users&&users.size == 1
-          account = users.first.account if users.first
-        end
+      	mobile = name
+      	users = Ecstore::User.where(:mobile=>mobile,:sms_validate=>'true',:supplier_id=>supplier_id)
+      	if users&&users.size == 1
+      		account = users.first.account if users.first
+      	end
       end
     end
 
     if account
 
-      if account.login_password[0] == "s"
-        encrypt = "s" + Digest::MD5.hexdigest("#{Digest::MD5.hexdigest(password)}#{account.login_name}#{account.createtime}")[0..30]
-        return account if encrypt == account.login_password
-      else
-        return account if Digest::MD5.hexdigest(password) == account.login_password
-      end
+    	if account.login_password[0] == "s"
+    		encrypt = "s" + Digest::MD5.hexdigest("#{Digest::MD5.hexdigest(password)}#{account.login_name}#{account.createtime}")[0..30]
+    		return account if encrypt == account.login_password
+    	else
+    		return account if Digest::MD5.hexdigest(password) == account.login_password
+    	end
     end
     nil
   end
 
-	def self.user_authenticate(name,password)
+  def self.user_authenticate(name,password)
 		#username
 		#account = self.where(:login_name=>name,:account_type=>"member").first
     #允许后台管理员登录前台
     account = self.where(:login_name=>name).first
 
-		unless account
+    unless account
 			#email
 			if /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i =~ name
 				email = name
@@ -265,19 +265,19 @@ class Ecstore::Account < Ecstore::Base
 
 	def follow_imodec
 		case self.auth_ext.provider
-			when 'weibo'
-				client = Weibo.new(:access_token=>self.auth_ext.access_token,
-									:expires_at=>self.auth_ext.expires_at)
-				return true if self.auth_ext.uid.to_s == Weibo.config.uid.to_s
-				ship = client.get('friendships/show.json',{:source_id=>client.config.uid,
-													:target_id=>self.auth_ext.uid})
-				ship.source.followed_by
-      when 'douban'
-        true
-      when 'weixin'
-        true
-			else
-				false
+		when 'weibo'
+			client = Weibo.new(:access_token=>self.auth_ext.access_token,
+				:expires_at=>self.auth_ext.expires_at)
+			return true if self.auth_ext.uid.to_s == Weibo.config.uid.to_s
+			ship = client.get('friendships/show.json',{:source_id=>client.config.uid,
+				:target_id=>self.auth_ext.uid})
+			ship.source.followed_by
+		when 'douban'
+			true
+		when 'weixin'
+			true
+		else
+			false
 		end
 	rescue Exception=>e
 		logger.error "check whether '#{self.login_name}' is following 'I-Modec'. because #{e}"
@@ -318,16 +318,36 @@ class Ecstore::Account < Ecstore::Base
 
 	after_save :follow_imodec!
 
+	def self.update_data
+		today = Time.now.midnight.to_i - 1.day
+		last_time = (Time.parse Ecstore::Summary.all[-1].history_date).to_i
+		days = (today - last_time) / 24 / 3600
+		if days > 0
+			(1..days - 1).each do |x|
+				Ecstore::Summary.new do |s|
+					this_day = last_time + x.day
+					this_day_over = this_day + 1.day
+					s.history_date = (Time.zone.at this_day).strftime('%F')
+					s.total_member_today = Ecstore::Account.where('createtime < ?', this_day_over).count
+					s.new_member_today = s.total_member_today - Ecstore::Account.where('createtime < ?', this_day).count
+					select_order = Ecstore::Order.where('createtime < ? AND createtime > ?', this_day_over, this_day)
+					s.new_order_today = select_order.count
+					s.new_order_payed_today = select_order.where('pay_status = ?', '1').count
+				end.save
+			end
+		end
+	end
+
 	private 
 	def encrypt_password
 		self.login_password =  "s" + Digest::MD5.hexdigest("#{Digest::MD5.hexdigest(self.login_password)}#{self.login_name}#{self.createtime}")[0..30]
 	end
 
 	def follow_imodec!
-		
+
 		if @following && self.auth_ext.provider == "weibo"
 			client = Weibo.new(:access_token=>self.auth_ext.access_token,
-							       :expires_at=>self.auth_ext.expires_at)
+				:expires_at=>self.auth_ext.expires_at)
 			client.post('friendships/create.json',:uid=>Weibo.config.uid)
 		end
 	rescue Exception=>e
